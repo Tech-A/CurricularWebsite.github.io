89f876c4fcf67dd99f1bc8b948a3300a5f94d380
Abbey <martinez.a@maristcollege.school.nz>
2021-05-29 21:06:26 +1200

    Remove button

--- website.js
+++ website.js
@@ -9,32 +9,6 @@ const slider = document.querySelector(".slider input");
     img.style.width = sliderVal + "%"
   }
 
-  //explore columns and grid
-  var elements = document.getElementsByClassName("column");
-            var i;
-            
-            function listView() {
-              for (i = 0; i < elements.length; i++) {
-                elements[i].style.width = "100%";
-              }
-            }
-            
-            function gridView() {
-              for (i = 0; i < elements.length; i++) {
-                elements[i].style.width = "50%";
-              }
-            }
-            
-            var container = document.getElementById("btnContainer");
-            var btns = container.getElementsByClassName("btn");
-            for (var i = 0; i < btns.length; i++) {
-              btns[i].addEventListener("click", function() {
-                var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-              });
-            }
-
 
 //main javascript project
 
