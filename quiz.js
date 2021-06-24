b4024af5a0d11e1b408f62b60fe1c281d5a55be3
Abbey <martinez.a@maristcollege.school.nz>
2021-06-25 10:47:46 +1200

    update quiz page

--- quiz.js
+++ quiz.js
@@ -1,2 +1,16 @@
-//main javascript project
+
+var questions = $('.question').length; //stores the number of questions
+var total = 0; //store the sum of the answers the user selected
+var avg = 0; //store the avg of the answers the user selected
+
+$('.answer').on('click', function(){
+    $(this).addClass('selected');
+    total += $(this).data('value');
+
+    console.log(total);
+ 
+
+
+})
+
 
