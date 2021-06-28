7ff8ef6922b8e7b4e6473a98ae47bc56f33219d4
Abbey <martinez.a@maristcollege.school.nz>
2021-06-29 10:33:48 +1200

    js

--- quiz.js
+++ quiz.js
@@ -4,6 +4,12 @@ var total = 0; //store the sum of the answers the user selected
 var avg = 0; //store the avg of the answers the user selected
 
 $('.answer').on('click', function(){
+    if($(this).parent().find('.selected').length > 0) {
+        total -= $(this).parent().find('.selected').data('value');
+        $(this).parent().find('.selected').removedClass('selected');
+    }
+
+
     $(this).addClass('selected');
     total += $(this).data('value');
 
