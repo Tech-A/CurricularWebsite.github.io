fd7ae7d29fc3317ce23a7e83c0f149b2aa0d24be
Abbey <martinez.a@maristcollege.school.nz>
2021-08-28 11:38:48 +1200

    change js

---
+++ quiz.js
@@ -0,0 +1,20 @@
+let q1a1 = document.querySelector("#question1answer1");
+let q1a2 = document.querySelector("#question1answer2");
+let q1Score = 0
+let totalScore = 0
+function q1a1Function() {
+    q1a1.style.background = "blue";
+    q1a2.style.background = "white";
+    q1Score = 1
+}
+function q1a2Function() {
+    q1a1.style.background = "white";
+    q1a2.style.background = "red";
+    q1Score = 2
+}
+function q1Submit() {
+    totalScore = totalScore + q1Score;
+    console.log(totalScore);
+}
+
+let averageScore = totalScore/5;
