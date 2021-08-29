10f85427dfe6114ab0b734478038a154d2555a88
Abbey <martinez.a@maristcollege.school.nz>
2021-08-29 17:53:26 +1200

    color

--- quiz.js
+++ quiz.js
@@ -14,10 +14,10 @@ function q1a1Function() {
     q1Score = 1
 }
 function q1a2Function() {
-    q1a1.style.background = "lightslategray";
-    q1a2.style.background = "goldenrod";
-    q1a3.style.background = "lightslategray";
-    q1a4.style.background = "lightslategray";
+    q1a1.style.background = "goldenrod";
+    q1a2.style.background = "lightslategray";
+    q1a3.style.background = "goldenrod";
+    q1a4.style.background = "goldenrod";
     q1Score = 2
 }
 function q1a3Function() {
