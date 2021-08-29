901aecf3e085c85c1f40864d58ccead7bdd0da0b
Abbey <martinez.a@maristcollege.school.nz>
2021-08-29 16:54:08 +1200

    make lighter

--- quiz.js
+++ quiz.js
@@ -7,7 +7,7 @@ let q1a4 = document.querySelector("#question1answer4");
 let q1Score = 0
 
 function q1a1Function() {
-    q1a1.style.background = "rgba(212, 174, 77, 0.747)";
+    q1a1.style.background = "rgba(216, 193, 137, 0.747)";
     q1a2.style.background = "lightslategray";
     q1a3.style.background = "lightslategray";
     q1a4.style.background = "lightslategray";
