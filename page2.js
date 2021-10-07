c8136cbbe86da446a0f9031ac3c2b9964385696a
Abbey <martinez.a@maristcollege.school.nz>
2021-10-07 15:08:43 +1300

    add sort by / query

---
+++ page2.js
@@ -0,0 +1,28 @@
+const modalWrapper = document.querySelector('.modal-wrapper');
+
+
+const tableUsers = document.querySelector('.table-users');
+
+let id;
+
+// Create element and render users
+const renderUser = doc => {
+const tr = `
+         <tr data-id='${doc.id}'>
+            <td>${doc.data().firstName}</td>
+            <td>${doc.data().curricular}</td>
+        </tr>
+        `;
+
+        tableUsers.insertAdjacentHTML('beforeend', tr);
+}
+
+
+
+// Get all users 
+db.collection('users').get().then(querySnapshot => {
+ querySnapshot.forEach( doc => {
+        renderUser(doc);
+    })
+});
+
